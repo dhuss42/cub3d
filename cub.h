4a792ad14be7bd0374e57a1535e388bd47db4af7
David Huss <dhuss@2-D-5.42heilbronn.de>
2024-12-19 11:28:36 +0100

    cub.h

--- cub.h
+++ cub.h
@@ -15,7 +15,7 @@
 
 # include "cub_structs.h"
 # include "./libft/libft.h"
-#include "MLX42/include/MLX42/MLX42.h"
+# include "MLX42/include/MLX42/MLX42.h"
 // MLX42/build/libmlx42.a -lglfw -framework Cocoa -framework OpenGL -framework IOKit
 // -> needed for compilation with MLX
 
