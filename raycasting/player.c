c0802b5890ef984861e8282f04a9b5c2a2d3cb2f
david <david.david@david.com>
2025-01-10 12:50:53 +0100

    added map, movable player and initial direction vector, also basic raycaster (not sure if it works yet

--- raycasting/player.c
+++ raycasting/player.c
@@ -37,36 +37,47 @@ void    determine_dir(t_cub *cub, char dir)
 {
     if (dir == 'N')
     {
-       cub->player_angle = 90.0f;
+       cub->player_angle = PI / 2;
     }
     else if (dir == 'E')
     {
-        cub->player_angle = 0.0f;
+        cub->player_angle = 0;
     }
     else if (dir == 'S')
     {
-        cub->player_angle = 270.0f;
+        cub->player_angle = 3 * PI / 2;
     }
     else if (dir == 'W')
     {
-        cub->player_angle = 180.0f;
+        cub->player_angle = PI;
     }
     cub->dir_player.x = cos(cub->player_angle);
-    cub->dir_player.x = sin(cub->player_angle);
+    cub->dir_player.y = sin(cub->player_angle);
+    printf(RED"dir player.x: %f /\n"WHITE, cub->dir_player.x);
+    printf(RED"dir player.y: %f /\n"WHITE, cub->dir_player.y);
 }
 // floats for continous values
 
 // If the player is facing East (0°), cos(0) gives 1 (along the x-axis), and sin(0) gives 0 (along the y-axis). 
 // So, the direction vector is (1, 0), which points straight to the right.
 
+void    determine_plane(t_cub *cub)
+{
+    t_vector    camera;
+
+    camera = cub->plane;
+    camera.x = cub->dir_player.y;
+    camera.y = cub->dir_player.x; // 0.66?
+
+    // inverse of direction vector
+    // needs to be variable based on the starting direction
+}
 
 int create_vectors(t_cub *cub)
 {
-    char dir;
-
-    dir = 'N'; // change later
-    cub->pos_player = find_position(cub->map, dir);
-    determine_dir(cub, dir);
-
+    printf("char dir: %c\n", cub->start_dir);
+    cub->pos_player = find_position(cub->map, cub->start_dir);
+    determine_dir(cub, cub->start_dir);
+    determine_plane(cub);
     return (0);
 }
--- raycasting/raycasting.h
+++ raycasting/raycasting.h
@@ -9,8 +9,20 @@
 # include <math.h>
 # include "../libft/libft.h"
 
-#define WIDTH 512
-#define HEIGHT 512
+#define WIDTH 1280
+#define HEIGHT 640
+#define PI 3.14159265358979323846  // Define Pi
+
+//---------------- COLORS --------------//
+# define RESET       "\033[0m"
+# define BLACK       "\033[30m"
+# define RED         "\033[31m"
+# define GREEN       "\033[32m"
+# define YELLOW      "\033[33m"
+# define BLUE        "\033[34m"
+# define MAGENTA     "\033[35m"
+# define CYAN        "\033[36m"
+# define WHITE       "\033[37m"
 
 typedef struct s_vector 
 {
@@ -18,17 +30,51 @@ typedef struct s_vector
     float   y;
 } t_vector;
 
+typedef struct s_point
+{
+    int x;
+    int y;
+} t_point;
+
 typedef struct s_cub
 {
     mlx_t       *mlx;
     mlx_image_t *image;
+    mlx_image_t *player_image;
+    mlx_image_t *dir_image;
+    mlx_image_t *drawn_walls;
+    mlx_image_t *rays;
     t_vector    pos_player;
     t_vector    dir_player;
+    t_vector    plane;
+    t_vector    ray_dir;
+    t_point     map_pos;
+    float       camera_x;
     float       player_angle;
+    float       first_side_x;
+    float       first_side_y;
+    float       dist_x;
+    float       dist_y;
+    float       plane_wall_dist;
+    int         step_x;
+    int         step_y;
+    int         hit;
+    int         side;
+    int         line_height;
+    int         line_start;
+    int         line_end;
+    uint32_t   colour;
     char        **map;
+    char        start_dir;
 } t_cub;
 
 int create_vectors(t_cub *cub);
+void    raycaster(t_cub *cub);
+
+void	draw_cell(mlx_image_t *image, int start_x, int start_y, int cell_size, uint32_t color);
+
+// -------helpers---------- //
+bool is_player(char c, t_cub *cub);
 
 #endif
 
